19784f43d54ff00cd319b4e4cd946428df460fd3
Koray Yanik <fumyuun@gmail.com>
2016-01-23 11:24:06 +0100

    Added some more documentation.

--- cell.vhd
+++ cell.vhd
@@ -1,40 +1,36 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
--- data_r
--- +--+
--- |  |----------------+
--- |  |--|\            |
--- |  |--| \--+--------|\
--- |  |--| /  |  _  +--|/--------
--- |  |--|/|  +-|_|-+
--- +--+   ||     |
---      select   clk
+----------
+-- A lut2 fpga cell. Internally, it has a 5-bit program memory with a latch.
+-- The select_i muxes the data over the last 4 bits. The first bit is
+-- to skip the latch.
+-- When mode_i is 1, it enters programming mode. Every clock cycle, prog_i is
+-- shifted in the memory, shifting one bit out of prog_i as well.
 
 entity cell is
     port (
-        clk_i : in std_logic;
-        rst_i : in std_logic;
-        select_i : in std_logic_vector(1 downto 0);
-        mode_i : in std_logic;
-        prog_i : in std_logic;
-        prog_o : out std_logic;
-        data_o : out std_logic
+        clk_i    : in  std_logic;
+        rst_i    : in  std_logic;
+        select_i : in  std_logic_vector(1 downto 0);
+        mode_i   : in  std_logic; -- '1' = programming mode
+        prog_i   : in  std_logic;  -- shift input
+        prog_o   : out std_logic; -- shift output
+        data_o   : out std_logic
     );
 end cell;
 
 architecture behav of cell is
     signal data_r     : std_logic_vector(4 downto 0);
     signal dmuxed_s   : std_logic;
-    signal skip_reg_s : std_logic;
-    signal reg_r      : std_logic;
+    signal skip_latch_s : std_logic;
+    signal latch_r      : std_logic;
 begin
 
-
     prog_o     <= data_r(0);
-    skip_reg_s <= data_r(4);
+    skip_latch_s <= data_r(4);
 
-    data_o <= dmuxed_s when skip_reg_s = '1' else reg_r;
+    data_o <= dmuxed_s when skip_latch_s = '1' else latch_r;
 
     comb: process(select_i, data_r)
     begin
@@ -51,12 +47,14 @@ begin
     begin
         if rising_edge(clk_i) then
             if rst_i = '1' then
-                reg_r  <= '0';
+                -- reset
+                latch_r  <= '0';
                 data_r <= "00000";
             else
-                reg_r <= dmuxed_s;
+                latch_r <= dmuxed_s;
 
                 if mode_i = '1' then
+                    -- shift in prog_i
                     data_r(0) <= data_r(1);
                     data_r(1) <= data_r(2);
                     data_r(2) <= data_r(3);
