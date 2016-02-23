b8477782e737a27e2f701b1dd426447983fe38c4
Koray Yanik <fumyuun@gmail.com>
2016-02-23 15:17:25 +0100

    Made compatible with lut2 and lut4's.
    Now creates truth table entries automatically for either 2 or 4 inputs.

--- tools/bitgen/main.cpp
+++ tools/bitgen/main.cpp
@@ -21,15 +21,13 @@ bool evaluate(AST_UnaryExpression &exp, ValueMap &valmap);
 bool evaluate(AST_Identifier &exp, ValueMap &valmap);
 
 ValueMap valmap_bind(IdMap &idmap, bool *vals);
+ValueMap valmap_bind(IdMap &id, unsigned int code);
 
 int main(int argc, char **argv)
 {
     IdMap idmap;
     ValueMap valmap;
-    bool ff[] = {false, false};
-    bool ft[] = {false, true};
-    bool tf[] = {true,  false};
-    bool tt[] = {true,  true};
+    int permutations_size = 0;
 
     yyparse();
     if (root == NULL) {
@@ -43,20 +41,21 @@ int main(int argc, char **argv)
         std::cout << it->first << ": " << it->second.size() << " occurrences" << std::endl;
     }
 
-    if (idmap.size() > 2) {
-        std::cout << "More than 2 identifiers not supported yet." << std::endl;
+    if (idmap.size() <= 2) {
+        permutations_size = 2;
+    } else if (idmap.size() <= 4) {
+        permutations_size = 4;
+    } else {
+        std::cout << "More than 4 identifiers not supported yet." << std::endl;
         return 2;
     }
+    std::cout << "Generating a lut" << permutations_size << "." << std::endl;
 
     std::cout << "0";
-    valmap = valmap_bind(idmap, ff);
-    std::cout << evaluate(*root, valmap);
-    valmap = valmap_bind(idmap, ft);
-    std::cout << evaluate(*root, valmap);
-    valmap = valmap_bind(idmap, tf);
-    std::cout << evaluate(*root, valmap);
-    valmap = valmap_bind(idmap, tt);
-    std::cout << evaluate(*root, valmap);
+    for(unsigned int i = 0; i < permutations_size * permutations_size; ++i) {
+        valmap = valmap_bind(idmap, i);
+        std::cout << evaluate(*root, valmap);
+    }
     std::cout << std::endl;
 
 
@@ -125,6 +124,15 @@ ValueMap valmap_bind(IdMap &id, bool *vals) {
     ValueMap valmap;
     for (auto it = id.begin(); it != id.end(); ++it) {
         valmap[it->first] = *vals++;
+    }
+    return valmap;
+}
+
+ValueMap valmap_bind(IdMap &id, unsigned int code) {
+    ValueMap valmap;
+    unsigned int mask = 1;
+    for (auto it = id.begin(); it != id.end(); ++it, mask <<= 1) {
+        valmap[it->first] = (code & mask) == mask;
     }
     return valmap;
 }
